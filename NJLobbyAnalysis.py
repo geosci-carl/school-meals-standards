55fa548b529b484e06be8a5e1385c45eafcabd04
Carl Fredrick G. Aquino <daproudpinoy@gmail.com>
2023-10-23 19:18:26 -0400

    fix names in lobby csv

--- NJLobbyAnalysis.py
+++ NJLobbyAnalysis.py
@@ -4,4 +4,12 @@ Created on Mon Oct 23 19:06:48 2023
 
 @author: Carl Fredrick G. Aquino (geosci.carl@gmail.com)
 """
+# import required packages
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
+# import CSV (data from https://www.elec.nj.gov/pdffiles/press_releases/pr_2023/pr_03092023.pdf)
+njlobby = pd.read_csv('data/NJLobbyingSectors20-22.csv')
+
+# plot
