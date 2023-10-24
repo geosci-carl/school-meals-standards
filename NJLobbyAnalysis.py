006bb497741d3b9ffb8b2bd05ecf8aa8232d6955
Carl Fredrick G. Aquino <daproudpinoy@gmail.com>
2023-10-23 20:47:43 -0400

    plot NJ Lobbying barplot

--- NJLobbyAnalysis.py
+++ NJLobbyAnalysis.py
@@ -7,9 +7,16 @@ Created on Mon Oct 23 19:06:48 2023
 # import required packages
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 
 # import CSV (data from https://www.elec.nj.gov/pdffiles/press_releases/pr_2023/pr_03092023.pdf)
-njlobby = pd.read_csv('data/NJLobbyingSectors20-22.csv')
+njlobby = pd.read_csv('data/NJLobbyingSectors20-22.csv', thousands=',')
 
 # plot
+plt.rcParams['font.size'] = '16'
+ax = njlobby.plot.bar(x='SECTOR', y='AMOUNT', figsize=(10,10))
+plt.legend(["Lobbying Dollars Spent Per Sector"])
+ax.set_xlabel('Sector')
+ax.set_ylabel('USD (millions)')
+
+# export plot
+plt.savefig("Plots/LobbyingBarplot.png", bbox_inches='tight')
